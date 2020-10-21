88949c50ba544cef29035944f1ded29e57ab17f2
Hugo Herter <git@hugoherter.com>
2020-10-21 17:38:24 +0200

    WIP: Refactor Monolithic Docker deployment in specific directory

---
+++ deployment/docker-demo/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Use this script to run the Docker image of PyAleph
+
+set -euo pipefail
+
+podman run --name pyaleph \
+  -p 8081:8081 \
+  -p 0.0.0.0:4024:4024 \
+  -p 0.0.0.0:4025:4025 \
+  -p 4001:4001 \
+  -p 5001:5001 \
+  -p 8080:8080 \
+  --mount type=tmpfs,destination=/var/log \
+  -v pyaleph-mongodb:/var/lib/mongodb \
+  -v pyaleph-ipfs:/var/lib/ipfs \
+  --rm -ti \
+  aleph.im/pyaleph-demo
